f9e3b714b0bbad1c2231906c811ed4cd013561bf
Harsh6530 <sharmaharsh634@kgpian.iitkgp.ac.in>
2025-06-05 22:08:18 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://thesuperchefs.com",
+  ],
 };
 
 export default nextConfig;
